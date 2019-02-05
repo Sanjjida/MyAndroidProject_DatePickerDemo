ea89ece59fbf986e397dcf9d6db472a3f98ba0d0
sanjida133 <afrinsanjida66@gmail.com>
2019-02-06 00:17:40 +0600

    committing source code

---
+++ app/src/main/java/com/example/windows/mydatepickerdemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.windows.mydatepickerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private TextView textView;
+private DatePicker datePicker;
+private Button selectButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView= (TextView) findViewById(R.id.buttonId);
+        selectButton= (Button) findViewById(R.id.buttonId);
+        datePicker=(DatePicker) findViewById(R.id.datePickerId);
+
+        textView.setText(currentDate());
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(currentDate());
+            }
+        });
+    }
+    String currentDate()
+    {
+
+        StringBuilder stringBuilder= new StringBuilder();
+        //StringBuilder.append("Current Date: ");
+        stringBuilder.append(datePicker.getDayOfMonth() + "/");
+        stringBuilder.append(datePicker.getMonth()+1 + "/");
+        stringBuilder.append(datePicker.getYear()+1 );
+        return stringBuilder.toString();
+
+    }
+}
